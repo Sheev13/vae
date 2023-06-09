643c2c3b81d842a999c79acf236551143cec41fa
Sheev13 <rochussent@gmail.com>
2023-06-09 10:40:45 +0100

    repo initialisation and first steps

---
+++ vae.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+
+from typing import Tuple, Optional, List
+
+class VAE(nn.Module):
+    def __init__(
+        self,
+    ):
+        super().__init__()
+        
+        
+class MLP(nn.Module):
+    def __init__(
+        self,
+        dims: List[int] = [1, 20, 20, 1],
+        nonlinearity: nn.Module = nn.ReLU(),
+    ):
+        super().__init__()
+        self.dims = dims
+        self.nonlinearity = nonlinearity
+        
+        net = []
+        for i in range(len(dims) - 1):
+            net.append(nn.Linear(dims[i], dims[i+1]))
+            if i < len(dims) - 2:
+                net.append(nonlinearity)
+                
+        self.net = nn.Sequential(*net)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.net(x)
+        
