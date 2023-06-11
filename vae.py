1c081c33da30f218e4701aa0b2153d2ae0fd771a
Sheev13 <rochussent@gmail.com>
2023-06-11 10:53:29 +0100

    more reading, minor changes

--- vae.py
+++ vae.py
@@ -3,13 +3,18 @@ from torch import nn
 
 from typing import Tuple, Optional, List
 
+
 class VAE(nn.Module):
     def __init__(
         self,
+        data_dim: int,
+        latent_dim: int,
+        inference_hidden_dims: List[int] = [20, 20],
+        q: str = "diagonal_gaussian",  # 'full_covariance_gaussian', 'inverse_autoregressive_flow',
     ):
         super().__init__()
-        
-        
+
+
 class MLP(nn.Module):
     def __init__(
         self,
@@ -19,15 +24,14 @@ class MLP(nn.Module):
         super().__init__()
         self.dims = dims
         self.nonlinearity = nonlinearity
-        
+
         net = []
         for i in range(len(dims) - 1):
-            net.append(nn.Linear(dims[i], dims[i+1]))
+            net.append(nn.Linear(dims[i], dims[i + 1]))
             if i < len(dims) - 2:
                 net.append(nonlinearity)
-                
+
         self.net = nn.Sequential(*net)
-        
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.net(x)
-        
